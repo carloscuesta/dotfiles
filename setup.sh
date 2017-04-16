58c936b4c8b8c43dcc1f466d33ae077dabb829a1
Carlos Cuesta <carloscuesta@me.com>
2017-04-16 20:16:43 +0200

    :fire: Add materialshell
    Signed-off-by: Carlos Cuesta <carloscuesta@me.com>

--- setup.sh
+++ setup.sh
@@ -23,9 +23,9 @@ for file in $execution_dir/.{zshrc,gitconfig,gitignore}; do
 done;
 unset file;
 
-# nico.zsh-theme -> $HOME/.oh-my-zsh/themes
-execute "cp -iv $execution_dir/nico.zsh-theme $HOME/.oh-my-zsh/themes/"
-print_success ".zshrc,.gitconfig,nico.zsh-theme copied!."
+# materialshell.zsh-theme -> $HOME/.oh-my-zsh/themes
+execute "cp -iv $execution_dir/materialshell.zsh-theme $HOME/.oh-my-zsh/themes/"
+print_success ".zshrc,.gitconfig,materialshell.zsh-theme copied!."
 
 ./osx/git-config.sh
 
