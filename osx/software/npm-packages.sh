f7bc08669ea1e14ee3807064e64d51903407d681
Carlos Cuesta <carloscuesta@me.com>
2020-08-15 20:27:16 +0200

    :fire: Update npm packages

--- osx/software/npm-packages.sh
+++ osx/software/npm-packages.sh
@@ -10,16 +10,8 @@ npm_packages() {
 		execute "npm update -g npm"
 
 		print_in_blue "npm packages"
-
-		npm_install "nodemon"
-		npm_install "gulp"
-		npm_install "webpack"
-		npm_install "node-sass"
-		npm_install "xo"
-		npm_install "sass-lint"
-    	npm_install "prettyjson"
-        npm_install "gitmoji-cli"
-        npm_install "react-native-cli"
+		npm_install "gitmoji-cli"
+		npm_install "react-native-cli"
 	else
 		print_error 'npm not installed (required to install node packages).'
 	fi
