6de5e3dee4f85a6cbfcf18fe2c11f7c1a5c979a9
Carlos Cuesta <carloscuesta@me.com>
2023-01-22 11:32:59 +0100

    ✨ Replace `atom` with `visual-studio-code`

--- osx/software/osx-apps.sh
+++ osx/software/osx-apps.sh
@@ -9,7 +9,7 @@ osx_apps() {
 	brew_install "caskroom/cask/brew-cask"
 	brew_install "caskroom/versions"
 
-  brew_cask_install "atom"
+	brew_cask_install "visual-studio-code"
 	brew_cask_install "iterm2"
 	brew_cask_install "rectangle"
 }
